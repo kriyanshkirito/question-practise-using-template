cf056786b39bd18c42a59c0c3dd55847d2516430
Kriyansh <draemon2709@gmail.com>
2026-03-18 19:22:48 +0530

    added

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ---------------- Fast IO ---------------- */
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+/* ---------------- Shortcuts ---------------- */
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+/* ---------------- Constants ---------------- */
+const int MOD = 1e9+7;
+const int INF = 1e9;
+
+/* ---------------- Useful Functions ---------------- */
+
+ll gcd(ll a, ll b){
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b){
+    return (a/gcd(a,b))*b;
+}
+
+bool isPrime(int n){
+    if(n<=1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+bool uniqueChar(string s){
+    unordered_set<char> st(s.begin(), s.end());
+    return s.size()==st.size();
+}
+
+/* ---------------- Problem Solve ---------------- */
+
+int maxsubarray(vector<int> & nums){
+int currsum=0;
+int maxsum=nums[0];
+for(int val:nums){
+   currsum=currsum+val;
+    maxsum=max(currsum,maxsum);
+     if(currsum<0){
+        currsum=0;
+ }
+}
+ return maxsum;
+}
+
+void solve(){
+
+
+    long long n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2;
+    }else{
+        cout<<-(n+1)/2;
+    }
+}  
+       
+    
+    
+
+
+
+/* ---------------- Main ---------------- */
+
+int main(){
+        solve();
+    
+
+} 
+    
+    
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Day 1-> I use template for first time and codeforces beautiful year question and
 Day 2-> solve first question of the day on codechef 
 Day->3 Took very long time (learn that when we have to swap array after particular interval then we use loop two times one loop for time interval and other for swapping elements)
 Day->4 Question completed 
+Day->5 question completed easy problem
