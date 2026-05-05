50f13e27e9bcb823a3264e37b2b56cacfa5a6028
kriyanshkirit <draemon2709@gmail.com>
2026-05-05 19:53:09 +0530

    done

---
+++ 43_A_Plus_or_Minus.cpp
@@ -0,0 +1,18 @@
+
+// https://codeforces.com/problemset/problem/1807/A
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c){
+            cout<<"+"<<endl;
+        }else{
+            cout<<"-"<<endl;
+        }
+
+    }
+}
