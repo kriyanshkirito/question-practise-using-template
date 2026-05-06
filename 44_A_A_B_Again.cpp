a33442178c0122a611e9142a0135b4c4aad3a804
kriyanshkirit <draemon2709@gmail.com>
2026-05-06 21:04:50 +0530

    done

---
+++ 44_A_A_B_Again.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/1999/A
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<(n%10)+(n/10)%10<<endl;
+
+    }
+}
